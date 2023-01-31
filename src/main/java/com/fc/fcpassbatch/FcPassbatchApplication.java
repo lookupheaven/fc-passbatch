b230743a6207c0a19c367fca23577a4395ce3d9c
voidmain <lookupheaven@gmail.com>
2023-01-31 22:42:15 +0900

    first. 2023.01.31

---
+++ src/main/java/com/fc/fcpassbatch/FcPassbatchApplication.java
@@ -0,0 +1,13 @@
+package com.fc.fcpassbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcPassbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcPassbatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/fcpassbatch/FcPassbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.fcpassbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcPassbatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
